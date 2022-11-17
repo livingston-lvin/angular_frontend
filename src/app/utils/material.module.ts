7ab75817944150d6510041d5d28f9e6a4445d6a0
livingston-lvin <livingstonlvin@gmail.com>
2022-11-17 11:14:54 +0530

    initial commit

---
+++ src/app/utils/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const Material: any = [
+  
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, Material],
+  exports: [Material],
+})
+export class MaterialModule {}
